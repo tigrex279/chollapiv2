c9a6fc11ed3271a114aff0d313bf1f6c47908b42
tigrex279 <tigrecito279@hotmail.es>
2023-02-01 14:14:15 +0100

    commit

---
+++ chollapiv2/src/main/java/com/example/demo/Chollapiv2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chollapiv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chollapiv2Application.class, args);
+	}
+
+}

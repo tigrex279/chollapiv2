f23c3dad138046cb9a51a123dc58bfda1d850aeb
tigrex279 <tigrecito279@hotmail.es>
2023-02-03 11:04:31 +0100

    probando cosas en service

--- chollapiv2/src/main/java/aad/Francisco/servicio/impl/OfertaServicioImpl.java
+++ chollapiv2/src/main/java/aad/Francisco/servicio/impl/OfertaServicioImpl.java
@@ -1,10 +1,20 @@
 package aad.Francisco.servicio.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import aad.Francisco.entidades.Oferta;
+import aad.Francisco.persistencia.OfertaDAO;
 
 @Transactional
 @Service
 public class OfertaServicioImpl {
-
+	 @Autowired
+	 OfertaDAO ofertaDao;
+	 
+	 public void comprobarOfertaDuplicada(Oferta oferta, Oferta ofertaComp) {
+		 
+		 
+	 }
 }
